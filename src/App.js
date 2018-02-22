b5a0b4ab0b97279db14c9f9bd1cc37aeee8e0eec
Sayan Mukherjee <sayan.mukherjee108@gmail.com>
2018-02-22 15:25:27 -0500

    auth flow to get token from spotify

--- src/App.js
+++ src/App.js
@@ -3,15 +3,23 @@ import SearchBar from './components/search_bar';
 import TrackList from './components/track_list';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1 className="page-header">Spotify Quick Lookup</h1>
-        <SearchBar />
-        <TrackList />
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            trackQuery: ""
+        };
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <h1 className="page-header">Spotify Quick Lookup</h1>
+                <SearchBar />
+                <TrackList />
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,11 +1,45 @@
 import React, { Component } from 'react';
+import Axios from 'axios';
 
 class SearchBar extends Component {
     constructor(props) {
         super(props);
 
-        this.state = { query: '' };
+        this.state = { 
+            query: 'jazz',
+            matchingTracks: []
+        };
     }
+
+    async getPermission() {
+        let authUrl = " https://cs-554-spotify-proxy.herokuapp.com/api/token";
+        const id = "5c81ebb84b61439ba96b4ea1f07502bd";
+        const secret = "2a2e332a64c0492898e69396221ed73a";
+        let key = id + ":" + secret;
+        key = btoa(key)
+        const authToken = await Axios.post(authUrl, {}, {
+            headers: { Authorization : "Basic " + key}
+        });
+
+        console.log("Token: ", authToken.data.access_token);
+        return authToken.data;
+    }
+
+    async getTracks(query) {
+        query = this.state.query;
+        let url = `https://api.spotify.com/v1/searchq=${query}&type=track`;
+        const response = await Axios.get(url);
+        console.log(response);
+        return response;
+    }
+
+    componentDidMount = async newProps => {
+        const authToken = await this.getPermission();
+        //const tracks = await this.getTracks(query, authToken);
+        // this.setState({
+        //     matchingTracks : tracks
+        // });
+    };
 
     render() {
         return (
@@ -13,7 +47,7 @@ class SearchBar extends Component {
                 <div className="input-group-prepend">
                     <span className="input-group-text" id="inputGroup-sizing-default">Search</span>
                 </div>
-                <input type="text" value={this.state.query} aria-describedby="inputGroup-sizing-default" />
+                <input type="text" aria-describedby="inputGroup-sizing-default" />
             </div>
         )
     }
